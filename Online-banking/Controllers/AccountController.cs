c36cddc54467dd39df3f8689cc981121d723ce3a
Aleksander Magnussen Dahling <s929575@stud.hioa.no>
2016-10-13 13:25:26 +0200

    Added AccountController and Account view

---
+++ Online-banking/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Online_banking.Models;
+
+namespace Online_banking.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Account(ModelContext.User inUser)
+        {
+            if(Session["loggedIn"] != null)
+            {
+                if (!(bool)Session["loggedIn"])
+                {
+                    RedirectToAction("index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                Session["loggedIn"] = false;
+                RedirectToAction("index");
+            }
+            return View();
+        }
+    }
+}
